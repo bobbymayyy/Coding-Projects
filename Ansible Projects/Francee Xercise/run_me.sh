e700d05ceee57ec443216b03952c85e9f59bf97d
Nathan <nathanenginee@gmail.com>
2023-11-18 00:49:19 -0500

    array fun

--- Ansible Projects/Francee Xercise/run_me.sh
+++ Ansible Projects/Francee Xercise/run_me.sh
@@ -135,9 +135,15 @@ else
     echo "Thats not a choice :("
 fi
 
-for i in $(echo $host_ip | awk -F. '{print $0}'); do
-    host_octets+=($i)
-done
+host_octets+=$(echo $host_ip | awk -F. '{print $1}')
+host_octets+=$(echo $host_ip | awk -F. '{print $2}')
+host_octets+=$(echo $host_ip | awk -F. '{print $3}')
+host_octets+=$(echo $host_ip | awk -F. '{print $4}')
+prox_octets+=$(echo $host_ip | awk -F. '{print $1}')
+prox_octets+=$(echo $host_ip | awk -F. '{print $2}')
+prox_octets+=$(echo $host_ip | awk -F. '{print $3}')
+prox_octets+=$(echo $host_ip | awk -F. '{print $4}')
+echo "${host_octets[-1]}"
 echo "${host_octets[-1]}"
 
 echo "Goodbye :)"
