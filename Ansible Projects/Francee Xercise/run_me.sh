5185a8d4dc22e1fc9001fde3dc437d9d5c18d306
bobbymayyy <nathanenginee@gmail.com>
2023-11-25 22:30:59 -0500

    choosing to deploy containers

--- Ansible Projects/Francee Xercise/run_me.sh
+++ Ansible Projects/Francee Xercise/run_me.sh
@@ -284,9 +284,9 @@ while [[ -z "$location" ]]; do
         ansible-playbook $ansible_check playbooks/01_configure_proxmox.yml
 
         ansible-playbook $ansible_check playbooks/11_deploy_opnsense.yml
-: '
-        ansible-playbook $ansible_check playbooks/12_deploy_c2.yml
 
+        ansible-playbook $ansible_check playbooks/12_deploy_c2.yml
+: '
         if [[ "$cluster_platform" =~ [pP] ]]; then
             ansible-playbook $ansible_check playbooks/13_deploy_securityonion.yml
         elif [[ "$cluster_platform" =~ [aA] ]]; then
