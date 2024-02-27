0159d932baab459ddcc44259a754905876fc2024
Nathan <nathanenginee@gmail.com>
2024-02-27 15:57:58 -0500

    push for eod

--- Python Projects/AutoVPN.py
+++ Python Projects/AutoVPN.py
@@ -9,14 +9,12 @@ import tkinter.font as tkFont
 #Function Definitions
 #----------------
 
-
 def wait_for_prompt(channel, prompt=">"):
     while True:
         output = channel.recv(4096).decode()
         print(output)
         if re.search(f"{prompt}\\s*$", output):
             break
-
 
 def configure_firewall(firewall_address, firewall_username, firewall_password, team_number, kit_number, pre_shared_key, peer_address, int_number, wan_address):
     # Create an SSH client with threading disabled
@@ -112,22 +110,15 @@ def configure_firewall(firewall_address, firewall_username, firewall_password, t
         # Close the SSH connection
         ssh.close()
 
-
 #==========================================================================================================================================================================================
-#Main Menu Definition
+#Main Menu
 #----------------
 
 class App:
     def __init__(self, root):
-        firewall_address=tk.StringVar()
-        firewall_username=tk.StringVar()
-        firewall_password=tk.StringVar()
-        team_number=tk.StringVar()
-        kit_number=tk.StringVar()
-        int_number=tk.StringVar()
-        wan_address=tk.StringVar()
-        peer_address=tk.StringVar()
-        pre_shared_key=tk.StringVar()
+#==========================================================================================================================================================================================
+#GUI Canvas
+#----------------
 
         #setting title
         root.title("AutoVPN")
@@ -140,224 +131,234 @@ class App:
         root.geometry(alignstr)
         root.resizable(width=False, height=False)
 
-        GLineEdit_358=tk.Entry(root)
-        GLineEdit_358["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times',size=10)
-        GLineEdit_358["font"] = ft
-        GLineEdit_358["fg"] = "#333333"
-        GLineEdit_358["justify"] = "center"
-        GLineEdit_358["text"] = "int_number"
-        GLineEdit_358.place(x=20,y=250,width=260,height=25)
-        GLineEdit_358["textvariable"] = int_number
+        firewall_address=tk.StringVar()
+        firewall_username=tk.StringVar()
+        firewall_password=tk.StringVar()
+        team_number=tk.StringVar()
+        kit_number=tk.StringVar()
+        int_number=tk.StringVar()
+        wan_address=tk.StringVar()
+        peer_address=tk.StringVar()
+        pre_shared_key=tk.StringVar()
 
-        GLineEdit_746=tk.Entry(root)
-        GLineEdit_746["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times',size=10)
-        GLineEdit_746["font"] = ft
-        GLineEdit_746["fg"] = "#333333"
-        GLineEdit_746["justify"] = "center"
-        GLineEdit_746["text"] = "team_number"
-        GLineEdit_746.place(x=20,y=200,width=260,height=25)
-        GLineEdit_746["textvariable"] = team_number
+#==========================================================================================================================================================================================
+#GUI Labels
+#----------------
 
-        GLineEdit_265=tk.Entry(root)
-        GLineEdit_265["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times',size=10)
-        GLineEdit_265["font"] = ft
-        GLineEdit_265["fg"] = "#333333"
-        GLineEdit_265["justify"] = "center"
-        GLineEdit_265["text"] = "peer_address"
-        GLineEdit_265.place(x=20,y=300,width=260,height=25)
-        GLineEdit_265["textvariable"] = peer_address
-
-        GLineEdit_624=tk.Entry(root)
-        GLineEdit_624["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times',size=10)
-        GLineEdit_624["font"] = ft
-        GLineEdit_624["fg"] = "#333333"
-        GLineEdit_624["justify"] = "center"
-        GLineEdit_624["text"] = "pre_shared_key"
-        GLineEdit_624.place(x=320,y=300,width=260,height=25)
-        GLineEdit_624["textvariable"] = pre_shared_key
-
-        GLineEdit_331=tk.Entry(root)
-        GLineEdit_331["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times',size=10)
-        GLineEdit_331["font"] = ft
-        GLineEdit_331["fg"] = "#333333"
-        GLineEdit_331["justify"] = "center"
-        GLineEdit_331["text"] = "wan_address"
-        GLineEdit_331.place(x=320,y=250,width=260,height=25)
-        GLineEdit_331["textvariable"] = wan_address
-
-        GLineEdit_881=tk.Entry(root)
-        GLineEdit_881["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times',size=10)
-        GLineEdit_881["font"] = ft
-        GLineEdit_881["fg"] = "#333333"
-        GLineEdit_881["justify"] = "center"
-        GLineEdit_881["text"] = "kit_number"
-        GLineEdit_881.place(x=320,y=200,width=260,height=25)
-        GLineEdit_881["textvariable"] = kit_number
-
-        GButton_76=tk.Button(root)
-        GButton_76["bg"] = "#e9e9ed"
-        ft = tkFont.Font(family='Times',size=10)
-        GButton_76["font"] = ft
-        GButton_76["fg"] = "#000000"
-        GButton_76["justify"] = "center"
-        GButton_76["text"] = "Ok"
-        GButton_76.place(x=120,y=350,width=70,height=25)
-        GButton_76["command"] = self.GButton_76_command
-
-        GButton_872=tk.Button(root)
-        GButton_872["bg"] = "#e9e9ed"
-        ft = tkFont.Font(family='Times',size=10)
-        GButton_872["font"] = ft
-        GButton_872["fg"] = "#000000"
-        GButton_872["justify"] = "center"
-        GButton_872["text"] = "Cancel"
-        GButton_872.place(x=410,y=350,width=70,height=25)
-        GButton_872["command"] = self.GButton_872_command
-
-        GLineEdit_343=tk.Entry(root)
-        GLineEdit_343["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times',size=10)
-        GLineEdit_343["font"] = ft
-        GLineEdit_343["fg"] = "#333333"
-        GLineEdit_343["justify"] = "center"
-        GLineEdit_343["text"] = "firewall_password"
-        GLineEdit_343.place(x=40,y=150,width=520,height=25)
-        GLineEdit_343["show"] = "*"
-        GLineEdit_343["textvariable"] = firewall_password
-
-        GLabel_513=tk.Label(root)
+        TitleLabel=tk.Label(root)
         ft = tkFont.Font(family='Times',size=15)
-        GLabel_513["font"] = ft
-        GLabel_513["fg"] = "#333333"
-        GLabel_513["justify"] = "center"
-        GLabel_513["text"] = "Welcome to AutoVPN; for automatically deploying a VPN connection. "
-        GLabel_513.place(x=20,y=10,width=560,height=70)
+        TitleLabel["font"] = ft
+        TitleLabel["fg"] = "#333333"
+        TitleLabel["justify"] = "center"
+        TitleLabel["text"] = "Welcome to AutoVPN; for automatically deploying a VPN connection. "
+        TitleLabel.place(x=20,y=10,width=560,height=70)
 
-        GLabel_813=tk.Label(root)
+        PasswordLabel=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
-        GLabel_813["font"] = ft
-        GLabel_813["fg"] = "#333333"
-        GLabel_813["justify"] = "center"
-        GLabel_813["text"] = "Please enter your password below:"
-        GLabel_813.place(x=200,y=125,width=200,height=25)
+        PasswordLabel["font"] = ft
+        PasswordLabel["fg"] = "#333333"
+        PasswordLabel["justify"] = "center"
+        PasswordLabel["text"] = "Please enter your password below:"
+        PasswordLabel.place(x=200,y=125,width=200,height=25)
 
-        GLabel_927=tk.Label(root)
+        TeamNumberLabel=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
-        GLabel_927["font"] = ft
-        GLabel_927["fg"] = "#333333"
-        GLabel_927["justify"] = "center"
-        GLabel_927["text"] = "Team Number"
-        GLabel_927.place(x=50,y=175,width=200,height=25)
+        TeamNumberLabel["font"] = ft
+        TeamNumberLabel["fg"] = "#333333"
+        TeamNumberLabel["justify"] = "center"
+        TeamNumberLabel["text"] = "Team Number"
+        TeamNumberLabel.place(x=50,y=175,width=200,height=25)
 
-        GLabel_683=tk.Label(root)
+        KitNumberLabel=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
-        GLabel_683["font"] = ft
-        GLabel_683["fg"] = "#333333"
-        GLabel_683["justify"] = "center"
-        GLabel_683["text"] = "Kit Number"
-        GLabel_683.place(x=350,y=175,width=200,height=25)
+        KitNumberLabel["font"] = ft
+        KitNumberLabel["fg"] = "#333333"
+        KitNumberLabel["justify"] = "center"
+        KitNumberLabel["text"] = "Kit Number"
+        KitNumberLabel.place(x=350,y=175,width=200,height=25)
 
-        GLabel_178=tk.Label(root)
+        WANInterfaceLabel=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
-        GLabel_178["font"] = ft
-        GLabel_178["fg"] = "#333333"
-        GLabel_178["justify"] = "center"
-        GLabel_178["text"] = "WAN Interface Number eg- 1/'X'"
-        GLabel_178.place(x=50,y=225,width=200,height=25)
+        WANInterfaceLabel["font"] = ft
+        WANInterfaceLabel["fg"] = "#333333"
+        WANInterfaceLabel["justify"] = "center"
+        WANInterfaceLabel["text"] = "WAN Interface Number eg- 1/'X'"
+        WANInterfaceLabel.place(x=50,y=225,width=200,height=25)
 
-        GLabel_399=tk.Label(root)
+        PeerIPLabel=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
-        GLabel_399["font"] = ft
-        GLabel_399["fg"] = "#333333"
-        GLabel_399["justify"] = "center"
-        GLabel_399["text"] = "Peer IP Address"
-        GLabel_399.place(x=50,y=275,width=200,height=25)
+        PeerIPLabel["font"] = ft
+        PeerIPLabel["fg"] = "#333333"
+        PeerIPLabel["justify"] = "center"
+        PeerIPLabel["text"] = "Peer IP Address"
+        PeerIPLabel.place(x=50,y=275,width=200,height=25)
 
-        GLabel_282=tk.Label(root)
+        WANIPLabel=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
-        GLabel_282["font"] = ft
-        GLabel_282["fg"] = "#333333"
-        GLabel_282["justify"] = "center"
-        GLabel_282["text"] = "WAN IP Address"
-        GLabel_282.place(x=350,y=225,width=200,height=25)
+        WANIPLabel["font"] = ft
+        WANIPLabel["fg"] = "#333333"
+        WANIPLabel["justify"] = "center"
+        WANIPLabel["text"] = "WAN IP Address"
+        WANIPLabel.place(x=350,y=225,width=200,height=25)
 
-        GLabel_511=tk.Label(root)
+        PSKLabel=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
-        GLabel_511["font"] = ft
-        GLabel_511["fg"] = "#333333"
-        GLabel_511["justify"] = "center"
-        GLabel_511["text"] = "Pre-Shared Key"
-        GLabel_511.place(x=350,y=275,width=200,height=25)
+        PSKLabel["font"] = ft
+        PSKLabel["fg"] = "#333333"
+        PSKLabel["justify"] = "center"
+        PSKLabel["text"] = "Pre-Shared Key"
+        PSKLabel.place(x=350,y=275,width=200,height=25)
 
-        GLineEdit_551=tk.Entry(root)
-        GLineEdit_551["borderwidth"] = "1px"
+        FirewallAddressLabel=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
-        GLineEdit_551["font"] = ft
-        GLineEdit_551["fg"] = "#333333"
-        GLineEdit_551["justify"] = "center"
-        GLineEdit_551["text"] = "firewall_address"
-        GLineEdit_551.place(x=20,y=100,width=260,height=25)
-        GLineEdit_551["textvariable"] = firewall_address
+        FirewallAddressLabel["font"] = ft
+        FirewallAddressLabel["fg"] = "#333333"
+        FirewallAddressLabel["justify"] = "center"
+        FirewallAddressLabel["text"] = "Firewall Address"
+        FirewallAddressLabel.place(x=50,y=75,width=200,height=25)
 
-        GLineEdit_26=tk.Entry(root)
-        GLineEdit_26["borderwidth"] = "1px"
+        FirewallUsernameLabel=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
-        GLineEdit_26["font"] = ft
-        GLineEdit_26["fg"] = "#333333"
-        GLineEdit_26["justify"] = "center"
-        GLineEdit_26["text"] = "firewall_username"
-        GLineEdit_26.place(x=320,y=100,width=260,height=25)
-        GLineEdit_26["textvariable"] = firewall_username
+        FirewallUsernameLabel["font"] = ft
+        FirewallUsernameLabel["fg"] = "#333333"
+        FirewallUsernameLabel["justify"] = "center"
+        FirewallUsernameLabel["text"] = "Firewall Username"
+        FirewallUsernameLabel.place(x=350,y=75,width=200,height=25)
 
-        GLabel_774=tk.Label(root)
+#==========================================================================================================================================================================================
+#GUI Entries
+#----------------
+
+        WANInterfaceEntry=tk.Entry(root, textvariable=int_number)
+        WANInterfaceEntry["borderwidth"] = "1px"
         ft = tkFont.Font(family='Times',size=10)
-        GLabel_774["font"] = ft
-        GLabel_774["fg"] = "#333333"
-        GLabel_774["justify"] = "center"
-        GLabel_774["text"] = "Firewall Address"
-        GLabel_774.place(x=50,y=75,width=200,height=25)
+        WANInterfaceEntry["font"] = ft
+        WANInterfaceEntry["fg"] = "#333333"
+        WANInterfaceEntry["justify"] = "center"
+        WANInterfaceEntry["text"] = "int_number"
+        WANInterfaceEntry.place(x=20,y=250,width=260,height=25)
 
-        GLabel_694=tk.Label(root)
+        TeamNumberEntry=tk.Entry(root, textvariable=team_number)
+        TeamNumberEntry["borderwidth"] = "1px"
         ft = tkFont.Font(family='Times',size=10)
-        GLabel_694["font"] = ft
-        GLabel_694["fg"] = "#333333"
-        GLabel_694["justify"] = "center"
-        GLabel_694["text"] = "Firewall Username"
-        GLabel_694.place(x=350,y=75,width=200,height=25)
+        TeamNumberEntry["font"] = ft
+        TeamNumberEntry["fg"] = "#333333"
+        TeamNumberEntry["justify"] = "center"
+        TeamNumberEntry["text"] = "team_number"
+        TeamNumberEntry.place(x=20,y=200,width=260,height=25)
 
-    def GButton_76_command(self):
+        PeerIPEntry=tk.Entry(root, textvariable=peer_address)
+        PeerIPEntry["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        PeerIPEntry["font"] = ft
+        PeerIPEntry["fg"] = "#333333"
+        PeerIPEntry["justify"] = "center"
+        PeerIPEntry["text"] = "peer_address"
+        PeerIPEntry.place(x=20,y=300,width=260,height=25)
 
-        fw_addr=firewall_address.get()
-        fw_user=firewall_username.get()
-        fw_pass=firewall_password.get()
-        team_num=team_number.get()
-        kit_num=kit_number.get()
-        int_num=int_number.get()
-        wan_addr=wan_address.get()
-        peer_addr=peer_address.get()
-        psk_key=pre_shared_key.get()
+        PSKEntry=tk.Entry(root, textvariable=pre_shared_key)
+        PSKEntry["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        PSKEntry["font"] = ft
+        PSKEntry["fg"] = "#333333"
+        PSKEntry["justify"] = "center"
+        PSKEntry["text"] = "pre_shared_key"
+        PSKEntry.place(x=320,y=300,width=260,height=25)
 
-        print(fw_addr)
-        print(fw_user)
-        print(fw_pass)
-        print(team_num)
-        print(kit_num)
-        print(int_num)
-        print(wan_addr)
-        print(peer_addr)
-        print(psk_key)
+        WANIPEntry=tk.Entry(root, textvariable=wan_address)
+        WANIPEntry["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        WANIPEntry["font"] = ft
+        WANIPEntry["fg"] = "#333333"
+        WANIPEntry["justify"] = "center"
+        WANIPEntry["text"] = "wan_address"
+        WANIPEntry.place(x=320,y=250,width=260,height=25)
+
+        KitNumberEntry=tk.Entry(root, textvariable=kit_number)
+        KitNumberEntry["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        KitNumberEntry["font"] = ft
+        KitNumberEntry["fg"] = "#333333"
+        KitNumberEntry["justify"] = "center"
+        KitNumberEntry["text"] = "kit_number"
+        KitNumberEntry.place(x=320,y=200,width=260,height=25)
+
+        PasswordEntry=tk.Entry(root, textvariable=firewall_password)
+        PasswordEntry["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        PasswordEntry["font"] = ft
+        PasswordEntry["fg"] = "#333333"
+        PasswordEntry["justify"] = "center"
+        PasswordEntry["text"] = "firewall_password"
+        PasswordEntry.place(x=40,y=150,width=520,height=25)
+        PasswordEntry["show"] = "*"
+
+        FirewallAddressEntry=tk.Entry(root, textvariable=firewall_address)
+        FirewallAddressEntry["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        FirewallAddressEntry["font"] = ft
+        FirewallAddressEntry["fg"] = "#333333"
+        FirewallAddressEntry["justify"] = "center"
+        FirewallAddressEntry["text"] = "firewall_address"
+        FirewallAddressEntry.place(x=20,y=100,width=260,height=25)
+
+        FirewallUsernameEntry=tk.Entry(root, textvariable=firewall_username)
+        FirewallUsernameEntry["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        FirewallUsernameEntry["font"] = ft
+        FirewallUsernameEntry["fg"] = "#333333"
+        FirewallUsernameEntry["justify"] = "center"
+        FirewallUsernameEntry["text"] = "firewall_username"
+        FirewallUsernameEntry.place(x=320,y=100,width=260,height=25)
+
+#==========================================================================================================================================================================================
+#GUI Buttons
+#----------------
+
+        OK_Button=tk.Button(root)
+        OK_Button["bg"] = "#e9e9ed"
+        ft = tkFont.Font(family='Times',size=10)
+        OK_Button["font"] = ft
+        OK_Button["fg"] = "#000000"
+        OK_Button["justify"] = "center"
+        OK_Button["text"] = "Ok"
+        OK_Button.place(x=120,y=350,width=70,height=25)
+        OK_Button["command"] = self.OK_ButtonAction
+
+        CANCEL_Button=tk.Button(root)
+        CANCEL_Button["bg"] = "#e9e9ed"
+        ft = tkFont.Font(family='Times',size=10)
+        CANCEL_Button["font"] = ft
+        CANCEL_Button["fg"] = "#000000"
+        CANCEL_Button["justify"] = "center"
+        CANCEL_Button["text"] = "Cancel"
+        CANCEL_Button.place(x=410,y=350,width=70,height=25)
+        CANCEL_Button["command"] = self.CANCEL_ButtonAction
+
+    def OK_ButtonAction(self):
+        fw_addr=firewall_address.get(root)
+        fw_user=firewall_username.get(root)
+        fw_pass=firewall_password.get(root)
+        team_num=team_number.get(root)
+        kit_num=kit_number.get(root)
+        int_num=int_number.get(root)
+        wan_addr=wan_address.get(root)
+        peer_addr=peer_address.get(root)
+        psk_key=pre_shared_key.get(root)
+
+        print("Firewall Address: " + fw_addr)
+        print("Firewall Username: " + fw_user)
+        print("Firewall Password: " + fw_pass)
+        print("Team Number: " + team_num)
+        print("Kit Number: " + kit_num)
+        print("Interface Number: " + int_num)
+        print("WAN Address: " + wan_addr)
+        print("Peer Address: " + peer_addr)
+        print("Pre-Shared Key: " + psk_key)
         #configure_firewall(firewall_address, firewall_username, firewall_password, team_number, kit_number, pre_shared_key, peer_address, int_number, wan_address)
 
-
-    def GButton_872_command(self):
+    def CANCEL_ButtonAction(self):
         sys.exit()
-
 
 #==========================================================================================================================================================================================
 #==========================================================================================================================================================================================
