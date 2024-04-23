57e847c2aefb161f441be5e9001d0810dbd64593
bobbymayyy <nathanenginee@gmail.com>
2024-04-23 00:47:00 -0400

    done for night, now on flow control

--- Bash Projects/autovpn.sh
+++ Bash Projects/autovpn.sh
@@ -38,12 +38,6 @@ pass_cover="" # - Veil the sensitive values -
 psk_cover="" # --------------------------------
 returncode=0        # - Initialize
 defaultitem="FW IP Address:"    # - Set default input field
-field_check() {
-    if [ -z "$fw_addr" ] || [ -z "$fw_user" ] || [ -z "$fw_pass" ] || [ -z "$team_num" ] || [ -z "$kit_num" ] || [ -z "$int_num" ] || [ -z "$wan_addr" ] || [ -z "$peer_addr" ] || [ -z "$psk_key" ]; then
-        DIALOG_TITLE="Welcome to AutoVPN. \Zb(Please fill in all fields.)\ZB"
-        break
-    fi
-}
 remove_zeros() {
     # Convert number to string to handle leading zeros
     local team_num="$1"
@@ -98,10 +92,12 @@ exec 3>&-
                     ;;
                 *)
                     # Check for empty fields and complain
-                    field_check
-                    octet=$(remove_zeros "$team_num")
-########################################################################
-                    DIALOG_TITLE="\Zb\Z1Welcome to AutoVPN.\ZB\Zn"
+                    if [ -z "$fw_addr" ] || [ -z "$fw_user" ] || [ -z "$fw_pass" ] || [ -z "$team_num" ] || [ -z "$kit_num" ] || [ -z "$int_num" ] || [ -z "$wan_addr" ] || [ -z "$peer_addr" ] || [ -z "$psk_key" ]; then
+                        DIALOG_TITLE="Welcome to AutoVPN. \Zb(Please fill in all fields.)\ZB"
+                    else
+                        octet=$(remove_zeros "$team_num")
+                        DIALOG_TITLE="\Zb\Z1Welcome to AutoVPN.\ZB\Zn"
+                    fi
                     ;;
             esac
             ;;
@@ -113,10 +109,12 @@ exec 3>&-
                     ;;
                 *)
                     # Check for empty fields and complain
-                    field_check
-                    octet=$(remove_zeros "$team_num")
-########################################################################
-                    DIALOG_TITLE="\Zb\Z2Welcome to AutoVPN.\ZB\Zn"
+                    if [ -z "$fw_addr" ] || [ -z "$fw_user" ] || [ -z "$fw_pass" ] || [ -z "$team_num" ] || [ -z "$kit_num" ] || [ -z "$int_num" ] || [ -z "$wan_addr" ] || [ -z "$peer_addr" ] || [ -z "$psk_key" ]; then
+                        DIALOG_TITLE="Welcome to AutoVPN. \Zb(Please fill in all fields.)\ZB"
+                    else
+                        octet=$(remove_zeros "$team_num")
+                        DIALOG_TITLE="\Zb\Z1Welcome to AutoVPN.\ZB\Zn"
+                    fi
                     ;;
             esac
             ;;
