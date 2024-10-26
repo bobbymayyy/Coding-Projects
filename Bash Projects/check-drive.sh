e8dfb198b23d2e157eef5561d4a7ab4d72cbb3dd
bobbymayyy <nathanenginee@gmail.com>
2024-10-25 21:15:08 -0400

    structuring

--- Bash Projects/check-drive.sh
+++ Bash Projects/check-drive.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #This is a script to hash or compare two folders to test build viability.
 clear
-
 echo "==================================================="
 
 meridiem=$(date | awk '{print $5}')
@@ -18,21 +17,22 @@ echo "Press any key to continue once done..."
 read -rsn1
 
 clear
-
 echo "==================================================="
 
 drive=$(journalctl --since '5m ago' | grep -Eo "sd\w" | awk -v RS=\n '{print $NF}')
 echo "Detecting drive as $drive; checking hashes..."
+echo "================"
+
 mkdir "/var/iso/test"
 ddsmiso=$(find / -type f -name ddsm-esxi.iso 2>/dev/null)
 mount -o loop $ddsmiso "/var/iso/test"
 cd "/var/iso/test"
 isobuild=$((find ddsm-esxi -type f -print0 | sort -z | xargs -0 sha1sum; find ddsm-esxi \( -type f -o -type d \) -print0 | sort -z | xargs -0 stat -c '%n %a') | sha1sum)
-echo "================"
 echo "This is the control:$isobuild"
+echo "---"
+
 cd /run/media/nerd/RHEL*
 drivebuild=$((find ddsm-esxi -type f -print0 | sort -z | xargs -0 sha1sum; find ddsm-esxi \( -type f -o -type d \) -print0 | sort -z | xargs -0 stat -c '%n %a') | sha1sum)
-echo "---"
 echo "This is your built drive:$drivebuild"
 echo "==="
 
@@ -41,6 +41,7 @@ if [[ $isobuild == $drivebuild ]]; then
 	echo "========================================="
 	echo "Press any key to return to launcher..."
 	read -rsn1
+	
 	umount "/var/iso/test"
 	rm -rf "/var/iso/test"
 	umount /run/media/nerd/RHEL*
@@ -50,11 +51,13 @@ else
 	echo "========================================="
 	echo "Press any key to view the differences between the builds..."
 	read -rsn1
+	
 	clear
 	diff -qrN "/var/iso/test/ddsm-esxi" /run/media/nerd/RHEL*/ddsm-esxi
 	echo "========================================="
 	echo "Press any key to return to launcher..."
 	read -rsn1
+	
 	umount "/var/iso/test"
 	rm -rf "/var/iso/test"
 	umount /run/media/nerd/RHEL*
