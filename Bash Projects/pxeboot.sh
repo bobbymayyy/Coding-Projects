018a76783e3d5d6470e96c77f1ef3967af5ebf28
bobbymayyy <nathanenginee@gmail.com>
2025-03-15 00:21:33 -0400

    initial pxeboot

---
+++ Bash Projects/pxeboot.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# ADD SERVICES ON FIREWALL
+firewall-cmd --add-service=dhcp
+firewall-cmd --add-service=tftp
+firewall-cmd --add-service=dns
+firewall-cmd --add-service=http
+firewall-cmd --reload
+
+# ENABLE SERVICES
+# HTTP for media serving
+systemctl start httpd
+# dnsmasq provides DHCP and TFTP
+systemctl start dnsmasq
+
+# MOUNT THE SELECTED ISO FOR HTTP
+mkdir -p /var/www/html/pxeboot-media
+mount -t iso9660 -o ro,loop $selected_iso /var/www/html/pxeboot-media
+
+
+# UNMOUNT THE ISO ONCE DONE
+umount /var/www/html/pxeboot-media
+rm -rf /var/www/html/pxeboot-media
+
+# DISABLE SERVICES
+systemctl stop httpd
+systemctl stop dnsmasq
+
+# REMOVE SERVICES ON FIREWALL
+firewall-cmd --remove-service=dhcp
+firewall-cmd --remove-service=tftp
+firewall-cmd --remove-service=dns
+firewall-cmd --remove-service=http
+firewall-cmd --reload
