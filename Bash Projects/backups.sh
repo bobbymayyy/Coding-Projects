3d57cbb9ce714f7d10a323bb78a7e77e0fa637d0
bobbymayyy <nathanenginee@gmail.com>
2025-03-14 21:22:23 -0400

    log backups

--- Bash Projects/backups.sh
+++ Bash Projects/backups.sh
@@ -9,3 +9,5 @@ yes | cp /etc/hosts $dest_directory
 yes | cp -r /etc/NetworkManager/system-connections/ $dest_directory
 yes | cp -r /srv/REPO/logs/ $dest_directory
 yes | cp -r /srv/scripts/ $dest_directory
+
+echo "Backup created: $(date)" >> $dest_directory'backups.log'
