e547fabe65e660728022bd7158f30abcee5ef60f
bobbymayyy <devonzane6@gmail.com>
2025-06-05 15:11:46 -0400

    start clone n check

--- Bash Projects/cloneNcheck.sh
+++ Bash Projects/cloneNcheck.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#This is a script to clone a removable SSD to an ISO, burn an ISO file to multiple physical drives at once, then hash and compare the mounted ISO with the built drives to test build viability.
+clear
 
-#This script is for cloning and checking build drives
-
+# Cursory ================
