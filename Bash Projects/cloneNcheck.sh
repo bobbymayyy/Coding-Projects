573e87ab6823b5f2847c966f98906922eb39a550
bobbymayyy <nathanenginee@gmail.com>
2025-10-23 20:20:34 -0400

    new script and update template

--- Bash Projects/cloneNcheck.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#This is a script to clone a removable SSD to an ISO, burn an ISO file to multiple physical drives at once, then hash and compare the mounted ISO with the built drives to test build viability.
-clear
-
-# Cursory ================
--- Bash Projects/updateNcheck.sh
+++ Bash Projects/updateNcheck.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#This is a template for effective bash scripting.
+#This is a script to update some drives you select and then validate build of the drive against the update used.
 clear
 set -euo pipefail
 IFS=$'\n\t'
